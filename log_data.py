a074b5b719ba17079727409140a39d8990dfa38d
LahiRumesh <lahrumesh28@gmail.com>
2022-04-07 09:48:06 +0530

    log data

---
+++ log_data.py
@@ -0,0 +1,40 @@
+import os
+from tabulate import tabulate
+import wandb
+
+
+
+def model_configs(cfg):
+    
+    header = f"Training {os.path.basename(cfg.data_dir)} Data Set"
+    print('-' * len(header))
+    print(header)
+    print('-' * len(header))
+    config_table = [["Model",cfg.model],
+                    ["Batch Size",cfg.batch_size],
+                    ["Image Size",cfg.image_size],
+                    ["Epochs",cfg.epochs],
+                    ["Learning Rate",cfg.learning_rate],
+                    ["Use Pretrained",cfg.pretrained]]
+    print(tabulate(config_table,tablefmt="fancy_grid"))
+
+
+
+def wandb_logs(phase,epoch_loss,epoch_acc):
+
+    if phase == 'train':
+        wandb.log({"train_loss": epoch_loss,
+                "train_accuracy" : epoch_acc
+                        })
+
+    elif phase == 'val':
+        wandb.log({"val_loss": epoch_loss,
+                    "val_accuracy" : epoch_acc
+                                })
+
+
+def get_accuracy(phase,epoch_loss,epoch_acc):
+    header = f'{phase} Loss: {epoch_loss:.4f} Accuracy: {epoch_acc:.4f}'
+    print('-' * len(header))
+    print(header)
+    print('-' * len(header))
